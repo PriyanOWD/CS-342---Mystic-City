2a4d1a36bc4e22cb35ac8eddc2a05dea9bc258ee
PriyanOWD <ps.527964@gmail.com>
2018-11-06 16:57:26 -0600

    Inital Commit

---
+++ Direction.java
@@ -0,0 +1,178 @@
+
+//Name: Priyan Sureshkumar
+//NetID: psures5
+//Class that defines the Direction objects.  It holds a two place Objects corresponding to where
+// the player orginates from and where the player is seeking to move on to.
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Direction
+{
+
+    private enum dirType
+    {
+
+        None("None", "None"), N("North", "N"), S("South", "S"), E("East", "E"), W("West", "W"), U("Up", "U"), 
+        D("Down","D"), NE("NorthEast", "NE"), NW("NorthWest", "NW"), SE("SouthEast", "SE"), SW("SouthWest","SW"), 
+        NNE("North-NorthEast", "NNE"), NNW("North-NorthWest", "NNW"), ENE("East-NorthEast", "ENE"), WNW("West-NorthWest", "WNW"),
+        ESE("East-SouthEast","ESE"), WSW("West-SouthWest", "WSW"), SSE("South-SouthEast", "SSE"), SSW("South-SouthWest", "SSW");
+
+        private final String text;
+        private final String abbreviation;
+
+        private dirType(String text, String abbreviation)
+        {
+            this.text = text;
+            this.abbreviation = abbreviation;
+        }
+
+        public String toString()
+        {
+            return text;
+        }
+
+        public boolean match(String str)
+        {
+            str = str.toLowerCase();
+            return (str.equals(text.toLowerCase()) || str.equals(abbreviation.toLowerCase()));
+        }
+    }
+
+    private final int D_ID;
+    private final dirType DIRECTION;
+    private final Pattern d_delim_Pattern = Pattern.compile("\r?\n");
+    private final Place SOURCE;
+    private final Place DESTINATION;
+    private final int lockPattern;
+    private boolean secure;
+
+    // private Vector<String> NORTH = new Vector<String>(); // = {"N"}; //Unused old implementation of mapping used
+    // input and directions
+
+    public Direction(int ID, Place from, Place to, String dir)
+    {
+        D_ID = ID;
+        SOURCE = from;
+        DESTINATION = to;
+        secure = false;
+        DIRECTION = dirType.None;
+        lockPattern = 1;
+    }
+
+    public Direction(Scanner scn)
+    {
+        Scanner sc = new Scanner(CleanLineScanner.gameFileParser(scn, d_delim_Pattern));
+
+        D_ID = Integer.valueOf(sc.next(("\\d+")));
+        SOURCE = Place.getPlacebyID(Integer.valueOf(sc.next(("\\d+"))));
+
+        DIRECTION = dirType.valueOf(sc.next(("\\w+")));
+
+        Integer LorUL_destID = Integer.valueOf(sc.next(("-?\\d+")));
+        if (LorUL_destID < 0)
+            secure = true;
+        else
+            secure = false;
+
+        DESTINATION = Place.getPlacebyID(Math.abs(LorUL_destID));
+        lockPattern = Integer.valueOf(sc.next(("\\d+")));
+        SOURCE.addDirection(this);
+        sc.close();
+    }
+
+    public boolean match(String s)
+    {
+
+        return DIRECTION.match(s);
+    }
+
+    public boolean leadsToNowhere() // Function that checks if direction leads to nowhere
+    {
+        return this.DESTINATION.checkID(0);
+    }
+
+    public void lock()
+    {
+        if (isLocked())
+            System.out.println("Already Locked");
+        else
+            System.out.println("Locking");
+        secure = true;
+        return;
+    }
+
+    public void unlock()
+    {
+        if (!isLocked())
+            System.out.println("Already Unlocked");
+        else
+            System.out.println("Unlocking");
+        secure = false;
+        return;
+    }
+
+    public boolean isLocked()
+    {
+        // return secure;
+        return secure == true;
+    }
+
+    public Place follow()
+    {
+        if (!isLocked())
+            return DESTINATION;
+
+        System.out.println("Attempted Location:  " + DESTINATION.name() + "   ...is Locked\n");
+        return SOURCE;
+    }
+
+    public void display()
+    {
+        System.out.print(DIRECTION + ": leads to ->" + DESTINATION.name());
+        if (isLocked())
+            System.out.println(" LOCKED");
+        else
+            System.out.println(" UNLOCKED");
+        return;
+    }
+
+    public void useKey(Artifact dirArtifact)
+    {
+        if (lockPattern == 0)
+        {
+            System.out.println("Path to " + DESTINATION.name() + " won't change \n");
+            return;
+        }
+
+        if (dirArtifact.Master())
+        {
+            if (isLocked())
+                unlock();
+            else
+                lock();
+            return;
+        }
+
+        if (dirArtifact.Match(this.lockPattern))
+        {
+            if (isLocked())
+                unlock();
+            else
+                lock();
+            System.out.println(DESTINATION.name() + "!\n");
+        } else
+        {
+            System.out.println(dirArtifact.name() + " not applicable when going " + DIRECTION + " from " + SOURCE.name()
+                    + " to " + DESTINATION.name() + "\n");
+        }
+        return;
+    }
+
+    public void print()
+    {
+        System.out.println("Direction ID:" + D_ID + "Cardinal Direction:" + DIRECTION + "Name of origination Place:"
+                + SOURCE.name() + "Name of target Place:" + DESTINATION.name());
+        return;
+    }
+}
---
+++ Place.java
@@ -0,0 +1,217 @@
+
+//Name: Priyan Sureshkumar
+//NetID: psures5
+//Class that defines the Place objects.  It holds a collection of artifacts and directions and characters relevant to the Place object
+//This Class essentially holds all Objects created for the game's function
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Place
+{
+
+    static final HashMap<Integer, Place> allPlacesMap; // Can optionally make hashmap public --- use
+                                                       // DoubleBraceInitialization??
+    static
+    {
+        allPlacesMap = new HashMap<Integer, Place>();
+    }
+
+    public static Place getPlacebyID(int id)
+    {
+        return allPlacesMap.get(id);
+    }
+
+    // Place variables. Final values.
+    private final Pattern g_delim_Pattern = Pattern.compile("\r?\n\r?\n");
+    private final String PNAME;
+    private final String PDESCRIPTION;
+    private final int PID;
+    private List<Direction> paths = new ArrayList<Direction>();
+    private List<Artifact> placeArtifacts = new ArrayList<Artifact>();
+    private List<Character> placeCharacters = new ArrayList<Character>();
+
+    public Place(int ID, String name, String description)
+    {
+        PID = ID;
+        PNAME = name;
+        PDESCRIPTION = description;
+        if (!allPlacesMap.containsKey(PID))
+            allPlacesMap.put(PID, this);
+    }
+
+    public Place(Scanner scn)
+    {
+        StringBuilder desc = new StringBuilder();
+        Scanner sc = new Scanner(CleanLineScanner.gameFileParser(scn, g_delim_Pattern));
+
+        PID = Integer.valueOf(sc.next(("\\d+")));
+        PNAME = sc.nextLine().trim();
+        sc.nextLine();
+        while (sc.hasNext())
+            desc.append(sc.nextLine() + "\n");
+        PDESCRIPTION = desc.toString();
+
+        if (!allPlacesMap.containsKey(PID))
+            allPlacesMap.put(PID, this);
+        sc.close();
+
+    }
+
+    public boolean checkID(int idToCheck) // Function used to cross check the Place object's ID with value passed
+    { // although it allows a person to just check every number against PID until the return value is true
+        return PID == idToCheck;
+    }
+
+    public String name()
+    {
+        return PNAME;
+    }
+/*
+    public String description()
+    {
+        return PDESCRIPTION;
+    }*/
+
+    public void addDirection(Direction dir)
+    {
+        if (dir.leadsToNowhere()) // Function to check if target place that direction leads to is a "Nowhere" and then
+                                  // auto locks it.
+            dir.lock();
+
+        paths.add(dir); // Adds Direction object to vector within Place object
+        return;
+    }
+
+    public Place followDirection(String pToGo)
+    {
+        // Place result = this; //Sets place to calling object in case no matches are found
+        for (Direction tmp : paths) // checks passed string value against every direction object in Place's vector
+                                    // member
+        {
+            if (tmp.match(pToGo)) // calls Directions match method to determine what to do
+                return tmp.follow(); // Always returns first Match found in the places Direction Vector
+            // return (result = tmp.follow());
+        }
+        return this;
+        // return result;
+    }
+
+    public void addArtifact(Artifact item)
+    {
+        placeArtifacts.add(item);
+    }
+
+    public void useKey(Artifact arti)
+    {
+        for (Direction tmp : paths)
+        {
+            tmp.useKey(arti);
+        }
+    }
+    
+    public void addCharacter(Character charVar)
+    {
+        placeCharacters.add(charVar);
+    }
+
+    public void removeCharacter(Character charVar)
+    {
+        placeCharacters.remove(charVar);
+    }
+    
+    /*
+     * public Artifact get(String itemName) 
+     * { 
+     *      for (Artifact tmp: placeArtifacts ) 
+     *      {
+     *          if((itemName.trim()).equals(tmp.name())) 
+     *          { 
+     *          Artifact retrieve = tmp; placeArtifacts.remove(tmp); 
+     *          return retrieve;
+     *          } 
+     *      } 
+     *      return null; 
+     * }
+     */
+
+    public Artifact removeArtifactByName(String itemName)
+    {
+        for (Artifact tmp : placeArtifacts)
+        {
+            if ((itemName.trim()).equals(tmp.name()))
+            {
+                Artifact retrieve = tmp;
+                placeArtifacts.remove(tmp);
+                return retrieve;
+            }
+        }
+        return null;
+    }
+    
+    public void display()
+    {
+        int count = 1;
+        System.out.println("\nCurrent Location ->" + PNAME + ":\n" + PDESCRIPTION);
+        if (!PNAME.equals("Exit"))
+        {
+            System.out.println(PNAME + " Directions:");
+            for (Direction tmp : paths)
+                tmp.display();
+            System.out.println("\n" + PNAME + " Artifacts:");
+            for (Artifact tmp : placeArtifacts)
+            {
+                System.out.print(count++ + ":");
+                tmp.display();
+            }
+            count = 0;
+            System.out.println("\nCharacters in " + PNAME + ":");
+            for (Character charVar : placeCharacters)
+            {
+                System.out.print(count++ + ":");
+                charVar.display();
+            }
+        }
+        System.out.println("\n");
+
+        return;
+    }
+
+    public void print()
+    {
+
+        System.out.println("Place Name: " + PNAME + "\nDescription:" + PDESCRIPTION + "\nID:" + PID);
+        System.out.println("Directions:");
+        for (Direction tmp : paths)
+            tmp.display();
+        return;
+    }
+
+    public void printAll()
+    {
+        int count = 1;
+        Collection<Place> everyPlace = Place.allPlacesMap.values();
+        for(Place tmpPlace: everyPlace)
+        {
+            tmpPlace.print();
+            System.out.println(PNAME + " Directions:");
+            for (Direction tmp : paths)
+                tmp.print();
+            System.out.println("\n" + PNAME + " Artifacts:");
+            for (Artifact tmp : placeArtifacts)
+            {
+                System.out.println(count++ + ":");
+                tmp.print();
+            }
+            count = 0;
+            System.out.println("\nCharacters in " + PNAME + ":");
+            for (Character charVar : placeCharacters)
+            {
+                System.out.print(count++ + ":");
+                charVar.print();
+            }
+        
+            System.out.println("\n");
+        }
+    }
+}
