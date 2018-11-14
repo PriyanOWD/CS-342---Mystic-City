691c22634ee9a360fd24300fb6611d45572e1394
Shyam Patel <39226683+mashy426@users.noreply.github.com>
2018-11-13 22:07:31 -0600

    Update Commands.java

--- Commands.java
+++ Commands.java
@@ -4,18 +4,6 @@
  * Date:   Nov 14, 2018
  */
 
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Joey Voorhees
- * NetID:  svoorh2
- * Date:   Nov 14, 2018
- */
-
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Priyan Sureshkumar
- * NetID:  psures5
- * Date:   Nov 14, 2018
- */
-
 // Commands class that inherits Move abstract class and, in its execution,
 // is used to display list of commands that can be used in the game
 public class Commands extends Move {
