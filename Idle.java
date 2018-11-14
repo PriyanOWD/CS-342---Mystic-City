81503ba748782f341aaf70dfd2c1ec68bf9bcd84
Shyam Patel <39226683+mashy426@users.noreply.github.com>
2018-11-13 22:13:36 -0600

    Update Idle.java

--- Idle.java
+++ Idle.java
@@ -4,19 +4,6 @@
  * Date:   Nov 14, 2018
  */
 
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Joey Voorhees
- * NetID:  svoorh2
- * Date:   Nov 14, 2018
- */
-
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Priyan Sureshkumar
- * NetID:  psures5
- * Date:   Nov 14, 2018
- */
-
-
 // Idle class that inherits Move abstract class and, in its execution,
 // is used to do nothing/terminate a move without doing anything
 public class Idle extends Move {
@@ -31,4 +18,4 @@ public class Idle extends Move {
         System.out.printf(charName + " does nothing\n");
         return true;
     }//end execute()
-}//end Quit class
+}//end Idle class
