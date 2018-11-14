57711f1c3ba78dc297cc122a34b8ad1aa332bcb0
Shyam Patel <39226683+mashy426@users.noreply.github.com>
2018-11-13 22:11:01 -0600

    Update GoPlayer.java

--- GoPlayer.java
+++ GoPlayer.java
@@ -4,18 +4,6 @@
  * Date:   Nov 14, 2018
  */
 
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Joey Voorhees
- * NetID:  svoorh2
- * Date:   Nov 14, 2018
- */
-
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Priyan Sureshkumar
- * NetID:  psures5
- * Date:   Nov 14, 2018
- */
-
 // GoPlayer class that inherits Go abstract class and, in its execution,
 // is used to allow a player to go from its current place to an adjacent place
 public class GoPlayer extends Go {
