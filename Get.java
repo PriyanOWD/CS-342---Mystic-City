cb78285ebb0843c133c335f9a0644332af4d0ba5
Shyam Patel <39226683+mashy426@users.noreply.github.com>
2018-11-13 22:09:15 -0600

    Update Get.java

--- Get.java
+++ Get.java
@@ -4,18 +4,6 @@
  * Date:   Nov 14, 2018
  */
 
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Joey Voorhees
- * NetID:  svoorh2
- * Date:   Nov 14, 2018
- */
-
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Priyan Sureshkumar
- * NetID:  psures5
- * Date:   Nov 14, 2018
- */
-
 // Get abstract class that inherits Move abstract class and utilizes the
 // command design pattern to encapsulate Get commands (GetNPC and GetPlayer)
 public abstract class Get extends Move {
