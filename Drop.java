ee10f027bb29b6e5b6c451e017ad48a578575a1b
Shyam Patel <39226683+mashy426@users.noreply.github.com>
2018-11-13 22:08:26 -0600

    Update Drop.java

--- Drop.java
+++ Drop.java
@@ -4,18 +4,6 @@
  * Date:   Nov 14, 2018
  */
 
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Joey Voorhees
- * NetID:  svoorh2
- * Date:   Nov 14, 2018
- */
-
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Priyan Sureshkumar
- * NetID:  psures5
- * Date:   Nov 14, 2018
- */
-
 // Drop abstract class that inherits Move abstract class and utilizes the
 // command design pattern to encapsulate Drop commands (DropNPC and DropPlayer)
 public abstract class Drop extends Move {
