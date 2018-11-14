3cf449947c2242355e3842455b59add92a23f599
Shyam Patel <39226683+mashy426@users.noreply.github.com>
2018-11-13 22:09:37 -0600

    Update GetNPC.java

--- GetNPC.java
+++ GetNPC.java
@@ -4,18 +4,6 @@
  * Date:   Nov 14, 2018
  */
 
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Joey Voorhees
- * NetID:  svoorh2
- * Date:   Nov 14, 2018
- */
-
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Priyan Sureshkumar
- * NetID:  psures5
- * Date:   Nov 14, 2018
- */
-
 // GetNPC class that inherits Get abstract class and, in its execution,
 // is used to allow an NPC to attempt to get an artifacts
 public class GetNPC extends Get {
