79665b825ebc5f0857dbcff1b8f9d2d4945b68ce
Shyam Patel <39226683+mashy426@users.noreply.github.com>
2018-11-13 22:09:54 -0600

    Update GetPlayer.java

--- GetPlayer.java
+++ GetPlayer.java
@@ -4,17 +4,6 @@
  * Date:   Nov 14, 2018
  */
 
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Joey Voorhees
- * NetID:  svoorh2
- * Date:   Nov 14, 2018
- */
-
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Priyan Sureshkumar
- * NetID:  psures5
- * Date:   Nov 14, 2018
- */
 import java.util.ArrayList;
 
 
