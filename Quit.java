4360976524a70a0952911c6e0f45c8313b038f42
Shyam Patel <39226683+mashy426@users.noreply.github.com>
2018-11-13 22:18:57 -0600

    Update Quit.java

--- Quit.java
+++ Quit.java
@@ -4,19 +4,6 @@
  * Date:   Nov 14, 2018
  */
 
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Joey Voorhees
- * NetID:  svoorh2
- * Date:   Nov 14, 2018
- */
-
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Priyan Sureshkumar
- * NetID:  psures5
- * Date:   Nov 14, 2018
- */
-
-
 // Quit class that inherits Move abstract class and, in its execution,
 // is used to quit or exit from the game
 public class Quit extends Move {
