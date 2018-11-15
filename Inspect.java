52904e385afccd19f25e20a0a7357228588cf413
Shyam Patel <mashy426@gmail.com>
2018-11-15 02:12:02 -0600

    Inspect ownership

--- Inspect.java
+++ Inspect.java
@@ -4,33 +4,18 @@
  * Date:   Nov 14, 2018
  */
 
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Joey Voorhees
- * NetID:  svoorh2
- * Date:   Nov 14, 2018
- */
 
-/* CS342 Term Project Part IV: Combination and Extension
- * Name:   Priyan Sureshkumar
- * NetID:  psures5
- * Date:   Nov 14, 2018
- */
-
-// Get abstract class that inherits Move abstract class to implement Inspect 
-//which commands checks the footprints in each place (GetNPC and GetPlayer)
+// Inspect abstract class that inherits Move abstract class to implement Inspect
+// which commands checks the footprints in each place
 public class Inspect extends Move {
-    // abstract method : execute "GET" command
-    
     Place charPlace;
     
-    public Inspect(Place p)
-    {
-        charPlace = p;
-    }
-    
+    public Inspect(Place p) { charPlace = p; }
+
+    // abstract method : execute "INSPECT" command
     public boolean execute()
     {
         charPlace.displayFPrints();
         return false;
     }
-}//end Get class
+}//end Inspect class
